7916e4a33ed61eb9ae484e2688aa73038acaefb9
YaniryFlorez <ymflorezrivera@gmail.com>
2025-01-16 00:49:02 +0000

    Taller21 pokemon

---
+++ src/components/PokemonImagen.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>componente Pokemon Imagen</h1>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Adivina mi pokemon de la imagen</h1>
+  <pokemonImagen/>
+  <pokemonOpciones/>
+
+</template>
+
+<script>
+import pokemonImagen from '../components/PokemonImagen.vue'
+import pokemonOpcines from '../components/PokemonOpcines.vue'
+
+export default {
+    components:{
+     pokemonImagen,
+     pokemonOpcines,
+    },
+
+}
+</script>
+
+<style>
+
+</style>
